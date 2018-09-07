eab6e2106419b6af65d152fcbdc37399b6524d9a
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-08 02:49:50 +0300

    added NetworkingServiceApi field to MainPresenter class and constructor.

--- app/src/main/java/study/rationalegoism/mvp_rx_study/presenter/MainPresenter.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/presenter/MainPresenter.java
@@ -1,12 +1,15 @@
 package study.rationalegoism.mvp_rx_study.presenter;
 
+import study.rationalegoism.mvp_rx_study.networking.NetworkingServiceApi;
 import study.rationalegoism.mvp_rx_study.presenter.contract.MainContract;
 
 public class MainPresenter implements MainContract.Presenter {
     private MainContract.View mView;
+    private NetworkingServiceApi networkingService;
 
-    public MainPresenter(MainContract.View mView) {
+    public MainPresenter(MainContract.View mView, NetworkingServiceApi networkingService) {
         this.mView = mView;
+        this.networkingService = networkingService;
     }
 
     @Override
