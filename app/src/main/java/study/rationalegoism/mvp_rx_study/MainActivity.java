7fb547e0a1091a599e9ad5cdd9d7d7699c500f9e
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-07 23:23:57 +0300

    added initViews() to MainActivity

--- app/src/main/java/study/rationalegoism/mvp_rx_study/MainActivity.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/MainActivity.java
@@ -2,17 +2,34 @@ package study.rationalegoism.mvp_rx_study;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import study.rationalegoism.mvp_rx_study.presenter.MainPresenter;
 import study.rationalegoism.mvp_rx_study.presenter.contract.MainContract;
+import study.rationalegoism.mvp_rx_study.view.adapter.RandomUserAdapter;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
     private MainContract.Presenter mPresenter = new MainPresenter(this);
-
+    private RecyclerView mRecycleView;
+    private final RandomUserAdapter mRandomUserAdapter = new RandomUserAdapter(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
     }
+
+    private void initView() {
+        mRecycleView = findViewById(R.id.rvRandomUsers);
+        mRecycleView.setLayoutManager(
+                new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL,
+                false));
+
+
+    }
+
 }
