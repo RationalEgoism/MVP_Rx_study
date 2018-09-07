d4edd058c5d42dc58b3eacd32fc28d2f6fa68dd2
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-08 02:50:32 +0300

    added initPresenter() to MainActivity

--- app/src/main/java/study/rationalegoism/mvp_rx_study/MainActivity.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/MainActivity.java
@@ -1,19 +1,21 @@
 package study.rationalegoism.mvp_rx_study;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
 import study.rationalegoism.mvp_rx_study.domain.entity.Result;
+import study.rationalegoism.mvp_rx_study.networking.NetworkingService;
+import study.rationalegoism.mvp_rx_study.networking.NetworkingServiceApi;
 import study.rationalegoism.mvp_rx_study.presenter.MainPresenter;
 import study.rationalegoism.mvp_rx_study.presenter.contract.MainContract;
 import study.rationalegoism.mvp_rx_study.view.adapter.RandomUserAdapter;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
-    private MainContract.Presenter mPresenter = new MainPresenter(this);
+    private MainContract.Presenter mPresenter;
     private RecyclerView mRecycleView;
     private final RandomUserAdapter mRandomUserAdapter = new RandomUserAdapter(this);
 
@@ -21,8 +23,13 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initView();
+        initPresenter();
+    }
+
+    private void initPresenter() {
+        NetworkingServiceApi networkingServiceApi = new NetworkingService(this);
+        mPresenter = new MainPresenter(this, networkingServiceApi);
     }
 
     private void initView() {
