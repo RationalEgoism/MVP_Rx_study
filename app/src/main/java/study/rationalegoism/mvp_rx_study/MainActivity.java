a41db21c99836225e6ca18eed6c024a314bcf8a7
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-07 20:57:07 +0300

    Implementation of MainContract.View and field MainContract.Presenter added to MainActivity class

--- app/src/main/java/study/rationalegoism/mvp_rx_study/MainActivity.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/MainActivity.java
@@ -3,7 +3,12 @@ package study.rationalegoism.mvp_rx_study;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import study.rationalegoism.mvp_rx_study.presenter.MainPresenter;
+import study.rationalegoism.mvp_rx_study.presenter.contract.MainContract;
+
+public class MainActivity extends AppCompatActivity implements MainContract.View {
+    private MainContract.Presenter mPresenter = new MainPresenter(this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
