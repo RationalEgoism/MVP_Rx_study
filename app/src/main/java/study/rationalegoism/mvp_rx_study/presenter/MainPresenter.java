eb5e4bad193ae6d6de92ab1d45d097bb349fd78c
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-08 02:57:58 +0300

    added getRandomUsers()'s body in MainPresenter

--- app/src/main/java/study/rationalegoism/mvp_rx_study/presenter/MainPresenter.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/presenter/MainPresenter.java
@@ -1,7 +1,12 @@
 package study.rationalegoism.mvp_rx_study.presenter;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import study.rationalegoism.mvp_rx_study.domain.entity.RandomUsers;
 import study.rationalegoism.mvp_rx_study.networking.NetworkingServiceApi;
 import study.rationalegoism.mvp_rx_study.presenter.contract.MainContract;
+import timber.log.Timber;
 
 public class MainPresenter implements MainContract.Presenter {
     private MainContract.View mView;
@@ -14,6 +19,20 @@ public class MainPresenter implements MainContract.Presenter {
 
     @Override
     public void getRandomUsers() {
+        Call<RandomUsers> randomUsersCall = networkingService.getRandomUsers();
+        randomUsersCall.enqueue(new Callback<RandomUsers>() {
+            @Override
+            public void onResponse(Call<RandomUsers> call, Response<RandomUsers> response) {
+                if(response.isSuccessful()){
+                    mView.displayRandomUsers(response.body().getResults());
+                }
+            }
 
+            //TODO make some notification to user
+            @Override
+            public void onFailure(Call<RandomUsers> call, Throwable t) {
+                Timber.i(t.getMessage());
+            }
+        });
     }
 }
