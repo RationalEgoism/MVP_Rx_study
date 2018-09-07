b206bb321945c1ac2a159ce839200bf095fc33fc
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-07 23:00:23 +0300

    created RandomUserAdapter class

---
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/view/adapter/RandomUserAdapter.java
@@ -0,0 +1,56 @@
+package study.rationalegoism.mvp_rx_study.view.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import study.rationalegoism.mvp_rx_study.R;
+import study.rationalegoism.mvp_rx_study.domain.entity.Result;
+
+public class RandomUserAdapter extends RecyclerView.Adapter<RandomUserAdapter.RandomUserViewHolder>{
+
+
+    private List<Result> resultList;
+
+    @NonNull
+    @Override
+    public RandomUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.random_peoples_item,
+                parent, false);
+        return new RandomUserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RandomUserViewHolder holder, int position) {
+        Result result = resultList.get(position);
+        holder.textViewCard.setText(result.getName().getFirst());
+        //TODO add pictureLoaderLib such as Picasso or Glide
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return resultList.size();
+    }
+
+    public void setResultList(List<Result> resultList) {
+        this.resultList = resultList;
+    }
+
+    public class RandomUserViewHolder extends RecyclerView.ViewHolder{
+        public ImageView imageViewCard;
+        public TextView textViewCard;
+
+        public RandomUserViewHolder(View itemView) {
+            super(itemView);
+            imageViewCard = itemView.findViewById(R.id.imageViewCard);
+            textViewCard = itemView.findViewById(R.id.textViewCard);
+        }
+    }
+}
