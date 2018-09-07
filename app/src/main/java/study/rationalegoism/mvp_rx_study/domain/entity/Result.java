9fbf3ab27ae1e26e0f920943742d5dedf24656e9
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-07 22:57:42 +0300

    added getter and setter to Result class

--- app/src/main/java/study/rationalegoism/mvp_rx_study/domain/entity/Result.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/domain/entity/Result.java
@@ -12,4 +12,20 @@ public class Result {
     @SerializedName("picture")
     @Expose
     private Picture picture;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Picture picture) {
+        this.picture = picture;
+    }
 }
