9567fc2969df8ad41ee27b76cebe95b8a92bcee2
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-10 23:18:13 +0300

    removed providing databaseName from DatabaseModule

--- app/src/main/java/study/rationalegoism/mvp_rx_study/data/database/DatabaseModule.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/data/database/DatabaseModule.java
@@ -13,20 +13,12 @@ import study.rationalegoism.mvp_rx_study.AppModule;
 //AppModule provides Context
 @Module(includes = AppModule.class)
 public class DatabaseModule {
-
-    @Provides
-    @Named(Config.DATABASE_NAME)
-    String provideDatabaseName(){
-        return Config.DATABASE_NAME;
-    }
-
     @Provides
     @Singleton
-    public RandomUsersDao provideRandomUsersDao(Context context,
-                                                @Named(Config.DATABASE_NAME) String databaseName){
+    public RandomUsersDao provideRandomUsersDao(Context context){
         return Room.databaseBuilder(context,
                 RandomUsersDb.class,
-                databaseName)
+                Config.DATABASE_NAME)
                 .build()
                 .randomUsersDao();
     }
