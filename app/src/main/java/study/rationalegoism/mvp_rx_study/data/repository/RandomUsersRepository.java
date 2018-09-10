a7288f7023616092e2e52be7ee6f5ad45c227194
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-10 17:23:33 +0300

    added caching data in local storage

--- app/src/main/java/study/rationalegoism/mvp_rx_study/data/repository/RandomUsersRepository.java
+++ app/src/main/java/study/rationalegoism/mvp_rx_study/data/repository/RandomUsersRepository.java
@@ -1,5 +1,6 @@
 package study.rationalegoism.mvp_rx_study.data.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Flowable;
@@ -8,27 +9,46 @@ import study.rationalegoism.mvp_rx_study.data.model.Person;
 public class RandomUsersRepository implements RandomUsersStore {
     private final RandomUsersStore localStorage;
     private final RandomUsersStore remoteStorage;
+    private List<Person> cache;
 
     public RandomUsersRepository(RandomUsersStore localStorage, RandomUsersStore remoteStorage) {
         this.localStorage = localStorage;
         this.remoteStorage = remoteStorage;
+
+        cache = new ArrayList<>();
     }
 
     @Override
     public Flowable<List<Person>> loadPersons(boolean refreshRequired) {
         if(refreshRequired)
-            return refresh(refreshRequired);
+            return refresh();
         else
-            return localStorage.loadPersons(false)
-                    .take(1)
-                    .filter(list -> !list.isEmpty());
+            // if cache is available, return it immediately
+            if(!cache.isEmpty())
+                return Flowable.just(cache);
+            else{
+                //else return data from local storage
+                return localStorage.loadPersons(false)
+                        .take(1)
+                        //get Flowable<Person> from Flowable<List<Person>> from caching then
+                        .flatMap(Flowable::fromIterable)
+                        .doOnNext(person -> cache.add(person))
+                        .toList()
+                        .toFlowable()
+                        .filter(list -> !list.isEmpty())
+                        //if local data is empty, get data from remote source instead
+                        .switchIfEmpty(refresh());
+            }
     }
 
 
-    private Flowable<List<Person>> refresh(boolean refreshRequired){
-        return remoteStorage.loadPersons(refreshRequired)
+    private Flowable<List<Person>> refresh(){
+        return remoteStorage.loadPersons(true)
                 .take(1)
-                .doOnNext(list -> clearData())
+                .doOnNext(list -> {
+                    cache.clear();
+                    clearData();
+                })
                 .flatMap(Flowable::fromIterable)
                 .doOnNext(this::addPerson)
                 .toList()
